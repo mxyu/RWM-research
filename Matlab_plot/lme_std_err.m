e8c8d929bc36869fc82890c4949724f01d526d24
Michelle Yu <michelle@Michelles-MacBook-Pro-3.local>
2018-04-23 13:11:47 -0400

    std error using Harry's math. Need to revise to include covariance.

--- Matlab_plot/lme_std_err.m
+++ Matlab_plot/lme_std_err.m
@@ -5,7 +5,7 @@ function CI = lme_std_err( lme )
     CI.x = [0:1:165];
     CI.std = [];
     for i = 1:length(CI.x)
-        CI.std(end+1) = sqrt(CI.x(i)^2 * lme.s_err^2 + lme.i_err^2) * 2 / sqrt(NUM_OBSERVATIONS);
+        CI.std(end+1) = sqrt(CI.x(i)^2 * lme.s_err^2 + lme.i_err^2) * 2;
     end
     CI.upper = lme.slope * CI.x + lme.intercept + CI.std;
     CI.lower = lme.slope * CI.x + lme.intercept - CI.std;
